36992d396e1741adad0f58ebf48766fb5cf2b491
Aseem Kohli <aseemko@gmail.com>
2023-02-08 20:27:41 -0500

    profile posts and avatars

--- app/routes.py
+++ app/routes.py
@@ -57,3 +57,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    # automatically return a 404 if the user is not found
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
