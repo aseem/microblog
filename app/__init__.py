97218e7d7bb5ca23e0f8a5c8ee275b4e7025aa72
Aseem Kohli <aseemko@gmail.com>
2023-02-18 06:26:27 -0500

    ch 13

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,12 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_babel import Babel
 from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +21,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app))
 
 # This import must come after app is created
 from app import routes, models, errors
@@ -51,3 +54,7 @@ if not app.debug:
         app.logger.addHandler(file_handler)
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
