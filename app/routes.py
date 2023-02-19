c347d06aeb29ff104ce355fc6237a9cf99870f81
Aseem Kohli <aseemko@gmail.com>
2023-02-19 05:55:52 -0500

    ch 14

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app.email import send_password_reset_email
+from langdetect import detect, LangDetectException
 
 @app.before_request
 def before_request():
@@ -19,7 +20,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
